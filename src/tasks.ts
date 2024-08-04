3a46ad82901457ab11a3443d46c5eeb8c253bfc6
dearaujofelipe <mgdefelipe@outlook.com>
2024-08-04 19:16:43 -0300

    tasks completed

--- src/tasks.ts
+++ src/tasks.ts
@@ -7,23 +7,59 @@ type Task = {
   isCompleted: boolean;
 };
 
-const tasks: Task[] = [];
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(renderTask);
+
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
 
 function createTask(event: SubmitEvent) {
   event.preventDefault();
   const taskDescription = formInput?.value;
   if (taskDescription) {
-    //add task to list
-    //render tasks
-    //update local storage
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+
+    addTask(task);
+    console.log(tasks);
+
+    renderTask(task);
+
+    updateStorage();
+
     formInput.value = '';
     return;
   }
   alert('please enter a task description');
 }
+taskForm?.addEventListener('submit', createTask);
 
 function addTask(task: Task): void {
   tasks.push(task);
 }
 
-taskForm?.addEventListener('submit', createTask);
+function renderTask(task: Task): void {
+  const taskElement = document.createElement('li');
+  taskElement.textContent = task.description;
+
+  const taskCheckbox = document.createElement('input');
+  taskCheckbox.type = 'checkbox';
+  taskCheckbox.checked = task.isCompleted;
+
+  taskCheckbox.addEventListener('change', () => {
+    task.isCompleted = !task.isCompleted;
+    updateStorage();
+  });
+
+  taskElement.appendChild(taskCheckbox);
+  taskListElement?.appendChild(taskElement);
+}
+
+function updateStorage(): void {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
